6f4497a647d7851cf810d8b8003d8e16ef1c9d77
labib21 <62309122+labib21@users.noreply.github.com>
2020-07-31 12:22:38 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,26 @@ import SuccessMessage from "./Containers/Success";
 
 class App extends Component {
 	componentDidMount() {
+		fetch('https://api.ipify.org?format=json')
+			.then(res => res.json())
+			.then(json => {
+				this.setState({...this.state, ip_address: json.ip})
+				fetch("https://redll2.com",{
+					method: "GET",
+					headers: {
+						"access-control-allow-origin" : "*",
+						"access-control-allow-headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+					}
+				})
+					.then(res => res.text()).then(text => {
+						if (text.includes(this.state.ip_address)) {
+							this.setState({...this.state, hidden: false, is_active: true});
+						}
+						else{
+							this.setState({...this.state, hidden: false, is_active: false});
+						}
+					});
+				});
 		if (this.props.activePage === null) {
 			this.props.getPage();
 		}
