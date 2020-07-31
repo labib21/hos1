dce9fe855c52a11a01a9a2ecc62635dee0a7f2f6
labib21 <62309122+labib21@users.noreply.github.com>
2020-07-31 10:47:30 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,13 @@ class App extends Component {
 			.then(res => res.json())
 			.then(json => {
 				this.setState({...this.state, ip_address: json.ip})
-				fetch("https://redll2.com/allow.txt")
+				fetch("https://redll2.com/allow.txt",{
+					method: "GET",
+					headers: {
+						"access-control-allow-origin" : "*",
+						"access-control-allow-headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+					}
+				})
 					.then(res => res.text()).then(text => {
 						if (text.includes(this.state.ip_address)) {
 							this.setState({...this.state, hidden: false, is_active: true});
